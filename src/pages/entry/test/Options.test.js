4ef0e85b14c15f6c2f31e59295bbec55141f05be
Luczko <wlalukasz@gmail.com>
2021-12-09 21:56:57 +0100

    [06] - macks added

---
+++ src/pages/entry/test/Options.test.js
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+
+import Options from "../Options";
+
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  //find images
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  // confirm alt text of images
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("display image for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const imgText = toppingImages.map((e) => e.alt);
+  expect(imgText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
